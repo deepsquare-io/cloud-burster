0711eddab7647a4beda2ae68e44904fb0f9f74c1
Frédéric Thomas <86131814+fredouric@users.noreply.github.com>
2023-06-13 16:33:37 +0200

    fix(shadow): resources not hardcoded (#18)

--- pkg/config/host.go
+++ pkg/config/host.go
@@ -5,6 +5,8 @@ import "github.com/squarefactory/cloud-burster/validate"
 type Host struct {
 	Name       string `yaml:"name,omitempty" validate:"omitempty"`
 	DiskSize   int    `yaml:"diskSize"       validate:"required"`
+	RAM        int    `yaml:"ram,omitempty" validate:"omitempty"`
+	GPU        int    `yaml:"gpu,omitempty" validate:"omitempty"`
 	FlavorName string `yaml:"flavorName"     validate:"required"`
 	ImageName  string `yaml:"imageName"      validate:"required"`
 	IP         string `yaml:"ip,omitempty"   validate:"omitempty,ip"`
